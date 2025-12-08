70b1c52dab9340d31f3ecb8bbda6973977f661b8
RxTeK³ <theodep34@gmail.com>
2025-12-08 13:51:07 +0100

    Create MyGameHelpers.h

---
+++ MyGameHelpers.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "CoreMinimal.h"
+#include "Engine/Engine.h"
+
+namespace MaLib
+{
+	inline void Print(const FString& Text, float Duration = 2.0f, FColor Color = FColor::Green)
+	{
+		if (GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(-1, Duration, Color, Text);
+		}
+	}
+    
+	inline void Log(const FString& Text)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s"), *Text);
+	}
+    
+	inline void PrintF(const FString& Format, float Value)
+	{
+		Print(FString::Printf(TEXT("%s: %.2f"), *Format, Value));
+	}
+}
